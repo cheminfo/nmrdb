65990147636b4408f2b495dd5f872afdaac6f049
Daniel Kostro <kostro.d@gmail.com>
2014-12-18 11:08:52 +0100

    Update service.php and views to use localStorage not pouchdb

--- service.php
+++ service.php
@@ -5,113 +5,77 @@
     <script src="lib/promise.js" type="text/javascript"></script>
 </head>
 <body>
-    <script type="text/javascript" charset="utf-8">
-        
+<script type="text/javascript" charset="utf-8">
+
     // data types mapping (default to string)
     var types = {
-      molfile: 'mol2d'
+        molfile: 'mol2d'
     };
-    
+
     // docIDs for service views, indexed by service name
     var views = {
-      'all-predictions': './predictor_all/',
-	  'nmr-1h-prediction': './new_predictor/',
-	  'cosy-prediction': './cosy/',
-	  'hmbc-prediction': './hmbc/',
-	  'nmr-13c-prediction': './13c/'
+        'all-predictions': './predictor_all/',
+        'nmr-1h-prediction': './new_predictor/',
+        'cosy-prediction': './cosy/',
+        'hmbc-prediction': './hmbc/',
+        'nmr-13c-prediction': './13c/'
     };
-    
+
     // Creating data object
     var data = {};
     try {
-      <?php foreach($_POST as $key=>$postvar): ?>
-      try {
-        data["<?php echo $key ?>"] = JSON.parse(<?php echo json_encode($postvar) ?>);
-      } catch(e) {
-        data["<?php echo $key ?>"] = <?php echo json_encode($postvar) ?>;
-      }
-      <?php endforeach; ?>
+        <?php foreach($_POST as $key=>$postvar): ?>
+        try {
+            data["<?php echo $key ?>"] = JSON.parse(<?php echo json_encode($postvar) ?>);
+        } catch(e) {
+            data["<?php echo $key ?>"] = <?php echo json_encode($postvar) ?>;
+        }
+        <?php endforeach; ?>
     } catch(e) {
-      console.log("Something went wrong");
+        console.log("Something went wrong");
     }
-    
+
     try {
-      <?php foreach($_GET as $key=>$postvar): ?>
-      try {
-        data["<?php echo $key ?>"] = JSON.parse(<?php echo json_encode($postvar) ?>);
-      } catch(e) {
-        data["<?php echo $key ?>"] = <?php echo json_encode($postvar) ?>;
-      }
-      <?php endforeach; ?>
+        <?php foreach($_GET as $key=>$postvar): ?>
+        try {
+            data["<?php echo $key ?>"] = JSON.parse(<?php echo json_encode($postvar) ?>);
+        } catch(e) {
+            data["<?php echo $key ?>"] = <?php echo json_encode($postvar) ?>;
+        }
+        <?php endforeach; ?>
     } catch(e) {
-      alert("Something went wrong");
+        alert("Something went wrong");
     }
 
 
     var service = data.name;
     delete data.name;
-    
+
     if(!service || typeof service !== "string" || !views[service]) {
         writeBody('Error: '+service+' service does not exist');
     } else {
-
-        var deletePouch;
-        if(Object.keys(data).length) {
-            deletePouch = new Promise(function(resolve){
-                PouchDB.destroy('external_infos').then(resolve);
-            });
-        }
-        else {
-            deletePouch = Promise.resolve();
-        }
-        
-        deletePouch.then(function(){
-            
-            var db = new PouchDB('external_infos');
-        
-            var docs = [];
-            for(var i in data) {
-                (function(i){
-                    docs.push(new Promise(function(resolve, reject){
-                        var rev;
-                        db.get(i, function(err, res){
-                            if(!err) {
-                                rev = res._rev;
-                            }
-                            var x = {
-                                _id: i,
-                                _rev: rev,
-                                type: types[i] || "string",
-                                value: data[i],
-								timestamp: new Date().getTime()
-                            };
-                            db.put(x, function(err){
-                                if(err)
-                                    reject();
-                                else
-                                    resolve();
-                            });
-                        });
-                    }));
-                })(i);
+        try{
+            if(window.localStorage) {
+                window.localStorage.setItem('external_infos', JSON.stringify(data));
             }
-            Promise.all(docs).then(function(){
-                writeBody('Document written to database. Redirecting...');
-                setTimeout(function() {
-                    window.location = views[service];
-                }, 500);
-            }, function(){
-                writeBody('Error: Could not write to database.');
-            });
-        
-        });
-    }
-    
-    function writeBody(text) {
-      var body = document.getElementsByTagName('body');
-      body[0].innerHTML += text;
+            writeBody('Document written to database. Redirecting...');
+            setTimeout(function() {
+                window.location = views[service];
+            }, 300);    
+        }
+        catch(err) {
+            writeBody('Error writing to local storage... We will redirect you anyway');
+            setTimeout(function() {
+                window.location = views[service];
+            }, 1000);
+        }
     }
 
-    </script>
+    function writeBody(text) {
+        var body = document.getElementsByTagName('body');
+        body[0].innerHTML += text;
+    }
+
+</script>
 </body>
 </html>
